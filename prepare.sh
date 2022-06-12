ef00b904b2f32a31431795a4c9e1039c6bdcf2d1
fc777888 <66010241+fc777888@users.noreply.github.com>
2022-06-12 17:40:22 +0800

    fix error:
    dpkg-reconfigure: unable to re-open stdin: No file or directory

--- prepare.sh
+++ prepare.sh
@@ -2,8 +2,16 @@
 
 # https://github.com/microsoft/vscode-dev-containers/blob/main/script-library/github-debian.sh#L184
 # https://github.com/cli/cli/issues/4634
+# https://serverfault.com/questions/500764/dpkg-reconfigure-unable-to-re-open-stdin-no-file-or-directory
 
 export DEBIAN_FRONTEND=noninteractive
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
 sudo apt-get update && sudo apt-get install -y git curl wget sudo apt-utils tzdata
 
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
