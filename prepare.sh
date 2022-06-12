fa25d860249ef699f8d1fd6a66c987e96d13a4be
fc777888 <66010241+fc777888@users.noreply.github.com>
2022-06-12 17:47:39 +0800

    fix tzdata installion.

--- prepare.sh
+++ prepare.sh
@@ -12,7 +12,7 @@ export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
-sudo apt-get update && sudo apt-get install -y git curl wget sudo apt-utils tzdata
+sudo apt-get update && DEBIAN_FRONTEND=noninteractive apt install -y tzdata && sudo apt-get install -y git curl wget sudo apt-utils
 
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
